0b51ff9406c789b5417807c4903b96207b667a56
Castanos28C <71905392+Castanos28C@users.noreply.github.com>
2023-02-18 17:10:38 -0500

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list_programmers/', views.list_programmers, name='list_programmers'),
     path('programmer_edit/', views.programmer_edit, name='programmer_edit'),
+    path('programmer_delete/<codigo>', views.programmer_delete, name='programmer_delete'),
     path('programmer_create/', views.programmer_create, name='programmer_create'),
     path('programmer/', views.programmer, name='programmer'),
 ]
